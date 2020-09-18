c08b55cacb0cbedd54fff89078dd45377bf3b940
Marcus <mcake2001@gmail.com>
2020-09-18 15:06:56 -0500

    --

---
+++ R/Myf.R
@@ -0,0 +1,16 @@
+#' @title Square function
+#'
+#' @description Takes a vector and returns the squared vector
+#'
+#' @param x
+#'
+#' @return a vector^2
+#' @export
+#'
+#' @examples
+#' x=1:30 myf(x)
+#'
+myf = function(x){
+  x^2
+
+}
