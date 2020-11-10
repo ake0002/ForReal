6d43e4a87630e95fbca4deee994879bf4e8e0831
Marcus <mcake2001@gmail.com>
2020-11-09 21:09:31 -0600

    yes

---
+++ R/ConfidenceInterval.R
@@ -0,0 +1,21 @@
+#' Confidence Interval
+#'
+#' @description This function will find a confidence interval for the mean of a given set of data
+#'
+#' @param x data
+#' @param confint confidence interval desired
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun myci(x,.95)
+myci = function(x, confint=.95){
+  ybar = mean(x)
+
+  t=qt(confint,length(d)-1)
+  ci=c()
+  ci[1]=mean(x)-t*sd(x)/sqrt(length(x))
+  ci[2]=mean(x)+t*sd(x)/sqrt(length(x))
+  return(ci)
+}
